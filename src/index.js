0a4cc84c9dc6b84d75ac1c775e5e54d9b302e92c
Doan-Thanh-Duy <thanhduy180697@gmail.com>
2019-10-09 17:27:16 +0700

    simple GUI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import BabylonScene from './BabylonScene';
 
-ReactDOM.render(< BabylonScene />, document.getElementById('root'));
+import {Router} from 'react-router-dom';
+
+ReactDOM.render(<App></App>, document.getElementById('root') );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
